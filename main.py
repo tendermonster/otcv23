d05e7c1aa994ef18da5b17136a76e8f9123e4713
Artiom Blinovas <25306286+tendermonster@users.noreply.github.com>
2023-06-19 22:58:40 +0200

    write docs, simplify main.py, add requirements

--- main.py
+++ main.py
@@ -9,29 +9,40 @@ from utils.util_processing import (
     video_to_frames,
 )
 
+# Url to the video you want to download
+URL_DEMO_VIDEO = "https://dlcv2023.s3.eu-north-1.amazonaws.com/demo_small.mp4"
+# Path to the folder where the video and frames will be saved
+PATH_MEDIA = "media/"
+# Path to the folder where the frames will be saved
+PATH_VIDEO_FRAMES = "media/video/"
+# Path to the model
+PATH_MODEL = "model_zoo/Swin2SR_CompressedSR_X4_48.pth"
+# Path to the folder where the results will be saved after running the model
+# Results will usually be saved to "results/swin2sr_*" directory
+PATH_RESULTS = "results/swin2sr_compressed_sr_x4"
+# Name of the output video
+PATH_OUTPUT_VIDEO = "demo_output.mp4"
+
 
 def download_and_process_video():
     """
     This method downloads a video of your choice and slices it into frames.
     """
     # Download video
-    url = "https://dlcv2023.s3.eu-north-1.amazonaws.com/demo_small.mp4"
-    saved_file_path = download_file(url, "media/")
+    url = URL_DEMO_VIDEO
+    saved_file_path = download_file(url, PATH_MEDIA)
     # pass it to the video_to_frames method
-    video_to_frames(saved_file_path, "media/video/")
+    video_to_frames(saved_file_path, PATH_VIDEO_FRAMES)
 
 
 if __name__ == "__main__":
     # 0. if needed download the model
-    if os.path.exists("model_zoo/Swin2SR_CompressedSR_X4_48.pth"):
+    if not os.path.exists(PATH_MODEL):
         model_url = "https://github.com/mv-lab/swin2sr/releases/download/v0.0.1/Swin2SR_CompressedSR_X4_48.pth"
-        model_path = "model_zoo/Swin2SR_CompressedSR_X4_48.pth"
-        download_file(model_url, model_path)
+        download_file(model_url, os.path.dirname(PATH_MODEL))
     # 1. download and process the video
     download_and_process_video()
     # 2. run the model and scale the frames
-    path_to_model = "model_zoo/Swin2SR_CompressedSR_X4_48.pth"
-    path_to_frames = "./media/video/"
     args = [
         "--task",
         "compressed_sr",
@@ -40,11 +51,11 @@ if __name__ == "__main__":
         "--training_patch_size",
         "48",
         "--model_path",
-        path_to_model,
+        PATH_MODEL,
         "--folder_lq",
-        path_to_frames,
+        PATH_VIDEO_FRAMES,
         "--save_img_only",
     ]
     main(args)
     # 3. convert scaled and restored frames back to a video
-    frames_to_video("results/swin2sr_compressed_sr_x4", "demo_output.mp4")
+    frames_to_video(PATH_RESULTS, PATH_OUTPUT_VIDEO)
